78db01e00df68a930479ac3fab41e7786832d9a3
Dmytro Bilokha <belohado@rambler.ru>
2016-03-16 22:36:22 +0200

    Added license and simple test

--- src/test/java/bilokhado/phonebook/PhonebookApplicationTests.java
+++ src/test/java/bilokhado/phonebook/PhonebookApplicationTests.java
@@ -1,12 +1,17 @@
 package bilokhado.phonebook;
 
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import bilokhado.phonebook.configuration.properties.XmlStorageProperties;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = PhonebookApplication.class)
@@ -14,8 +19,16 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @TestPropertySource("classpath:/testxml.properties")
 public class PhonebookApplicationTests {
 
+	public static final String TEST_CONFIG_FILENAME = "/usr/home/dimon/temp/testusers.xml";
+	public static final boolean TEST_XML_FORMATTED = true;
+	
+	@Autowired
+	XmlStorageProperties properties;
+	
 	@Test
-	public void contextLoads() {
+	public void xmlStoragePropertiesTest() {
+		assertThat(properties.isFormatted(), equalTo(TEST_XML_FORMATTED));
+		assertThat(properties.getFileName(), equalTo(TEST_CONFIG_FILENAME));
 	}
 
 }
